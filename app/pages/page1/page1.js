a099b6fcc697c09df6ac302eaaf60ef847d501a0
kirkiki <ronanpsf@gmail.com>
2016-03-25 14:28:22 +0100

    hello js

--- app/pages/page1/page1.js
+++ app/pages/page1/page1.js
@@ -1,7 +1,9 @@
 import {Page} from 'ionic-angular';
-
+import {Hello} from './hello';
 
 @Page({
-  templateUrl: 'build/pages/page1/page1.html'
+  templateUrl: 'build/pages/page1/page1.html',
+  directives:[Hello]
 })
 export class Page1 {}
+
